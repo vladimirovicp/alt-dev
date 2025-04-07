b2acaf0855a57bcd813cf48d90a33dfa8148ad43
vladimirovicp <vladimirovicp70@gmail.com>
2025-04-07 17:57:57 +0400

    feat: admc add dependencies

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,12 +3,11 @@ const admcMenu = [
     { text: 'Лицензия', link: '/projects/admc/license'},
     { text: 'Релизы', link: '/projects/admc/releases'},
     { text: 'Ссылка на исходники', link: '/projects/admc/link-to-sources/'},
-    { text: 'Инструменты', link: '/projects/admc/tools/'},
+    { text: '+Зависимости', link: '/projects/admc/dependencies/'},
     { text: 'Документация', link: '/projects/admc/documentation/'},
     { text: 'Полезная информация', link: '/projects/admc/useful-information/'},
+    { text: 'Инструменты', link: '/projects/admc/tools/'},
     { text: 'Участники проекта', link: '/projects/admc/project-participants/'},
-
-    
 ]
 
 module.exports = {
--- docs/projects/admc/dependencies/index.md
+++ docs/projects/admc/dependencies/index.md
@@ -1,1 +1,16 @@
 # Зависимости
+
+- rpm-macros-cmake
+- cmake
+- gcc-c++
+- qt5-base-devel
+- qt5-tools-devel
+- cmake-modules
+- samba-devel
+- libldap-devel
+- libsasl2-devel
+- libsmbclient-devel
+- qt5-base-common
+- doxygen
+- libuuid-devel
+- libkrb5-devel
