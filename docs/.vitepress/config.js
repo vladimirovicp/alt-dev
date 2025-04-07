6a504f710b159facb58a9f513a9a5d17645664c3
vladimirovicp <vladimirovicp70@gmail.com>
2025-04-07 17:46:13 +0400

    feat: структурирование, настройки, начало заполнение проекта

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,18 +1,56 @@
+const admcMenu = [
+    { text: 'Описание проекта', link: '/projects/admc/'},
+    { text: 'Лицензия', link: '/projects/admc/license'},
+    { text: 'Релизы', link: '/projects/admc/releases'},
+    { text: 'Ссылка на исходники', link: '/projects/admc/link-to-sources/'},
+    { text: 'Инструменты', link: '/projects/admc/tools/'},
+    { text: 'Документация', link: '/projects/admc/documentation/'},
+    { text: 'Полезная информация', link: '/projects/admc/useful-information/'},
+    { text: 'Участники проекта', link: '/projects/admc/project-participants/'},
+
+    
+]
+
 module.exports = {
     lang: 'ru-RU',
     title: 'AltLinux.dev',
     logo: '/logo.png',
+    head: [
+        ['link', 
+            { 
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico' 
+            }
+        ]
+    ],
 
     themeConfig: {
         nav: nav(),
+        sidebar: {
+            '/projects/admc': admcMenu,
+        },
+        outline: { label: 'Содержание страницы' },
+        docFooter: {
+            prev: 'Предыдущая страница',
+            next: 'Следующая страница'
+          },
+
+        darkModeSwitchLabel: 'Оформление',
+        lightModeSwitchTitle: 'Переключить на светлую тему',
+        darkModeSwitchTitle: 'Переключить на тёмную тему',
+        sidebarMenuLabel: 'Меню',
+        returnToTopLabel: 'Вернуться к началу',
+        langMenuLabel: 'Изменить язык',
+        skipToContentLabel: 'Перейти к содержимому'
     }
+
+    
 }
 
 
 function nav(){
     return [
-
-        
         {
             text: 'Главная',
             link: '/',
